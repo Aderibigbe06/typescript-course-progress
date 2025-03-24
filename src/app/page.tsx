6e9428cef810beb5af996f1b9f7e2b5c9342dad2
Aderibigbe Fatima <aderibigbeoyindamola58@gmail.com>
2025-03-24 20:54:14 +0100

    Fixed build errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-// export default async function Page() {
-//   await new Promise((resolve) => setTimeout(resolve, 1000));
-//   return (
-//     <div>Home Page</div>
-//   );
-// }
+export default async function Page() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return (
+    <div>Home Page</div>
+  );
+}
 
-import { PageProps } from "../../.next/types/app/page";
+// import { PageProps } from "../../.next/types/app/page";
 
 
 // export default async function Page({ params }: { params: { topic: string } }) {
@@ -15,25 +15,3 @@ import { PageProps } from "../../.next/types/app/page";
 //       <div>Topic Page: {params.topic}</div>
 //   );
 // }
-
-
-
-
-// interface PageProps {
-//   params: {
-//     topic: string;
-//   };
-// }
-
-// export default function Page({ params }: PageProps) {
-//   return (
-//     <div>
-//       <h1>Topic: {params.topic}</h1>
-//     </div>
-//   );
-// }
-
-export default async function Page({ params }: PageProps) { // ✅ Use corrected PageProps
-  console.log(params?.home); 
-  return <div>Home Page</div>;
-}
